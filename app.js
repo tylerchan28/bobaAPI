75c17a3824922e9ddb053967e56751389ba675d4
Tyler Chan <tylerchan28@gmail.com>
2021-08-25 12:21:23 -0700

    Add cors with credentials

--- app.js
+++ app.js
@@ -81,8 +81,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
-app.options('*', cors())
+// app.use(cors());
+// app.options('*', cors())
+app.use(cors({
+  origin: "https://boba-guide-tyler.herokuapp.com",
+  credentials: true,
+})
+);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reviews', reviewsRouter);
